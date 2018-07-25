320fe9147e5fdd8a607def094a8c93d6fbb5c868
huzi <2736523504@qq.com>
2018-07-25 15:33:14 +0800

    第一次提交测试文件到GitHub

--- src/main/java/com/kapphk/pms/util/serialnum/SerialNumber.java
+++
@@ -1,9 +0,0 @@
-package com.kapphk.pms.util.serialnum;
-
-public abstract class SerialNumber {
-	public synchronized String getSerialNumber() {  
-        return process();  
-    }  
-  
-    protected abstract String process();
-}
